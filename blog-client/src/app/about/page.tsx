569a9d334043647547ee58b4d33b18093c833862
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-01-13 20:55:21 +0600

    module 31, vedio 5, done!

--- blog-client/src/app/about/page.tsx
+++ blog-client/src/app/about/page.tsx
@@ -1,6 +1,11 @@
+export const dynamic = "force-dynamic";
+
 export default async function AboutPage() {
+    //* For simulating load time
     await new Promise((resolve) => setTimeout(resolve, 4000));
-    throw new Error("Something went wrong");
+
+    //* For simulating error
+    // throw new Error("Something went wrong");
     return (
         <div>
             <h1>This is About Page</h1>
