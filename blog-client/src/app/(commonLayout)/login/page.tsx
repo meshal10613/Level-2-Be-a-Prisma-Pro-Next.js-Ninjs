5723ca1cf0eb7cdd2fbbc0e118b904e4e48eebb4
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-01-17 15:10:08 +0600

    module 32, vedio 10, better auth installation done!

--- blog-client/src/app/(commonLayout)/login/page.tsx
+++ blog-client/src/app/(commonLayout)/login/page.tsx
@@ -1,7 +1,11 @@
-export default function LoginPage() {
-	return(
-		<div>
-			<h1>This is Login Page</h1>
-		</div>
-	)
+import { LoginForm } from "@/components/modules/authentication/login-form"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
