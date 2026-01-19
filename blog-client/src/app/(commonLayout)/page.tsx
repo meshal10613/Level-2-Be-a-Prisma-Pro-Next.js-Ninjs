6d9740999c0042245c64b3c2fac4e4389490da2f
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-01-19 12:56:15 +0600

    module 33, vedio 7, done!

--- blog-client/src/app/(commonLayout)/page.tsx
+++ blog-client/src/app/(commonLayout)/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
+// import { userService } from "@/services/user.service";
 
 export default async function Home() {
-    const {data, error} = await userService.getSession();
-    console.log(data, error)
+    // const {data, error} = await userService.getSession();
+    // console.log(data, error)
 
     return (
         <div>
