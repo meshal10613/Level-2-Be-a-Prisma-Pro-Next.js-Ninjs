fd4cf62960871d857bd2c51f1c73fb43f595abd2
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-01-19 12:46:44 +0600

    module 33, vedio 6, done!

--- blog-client/src/services/user.service.ts
+++ blog-client/src/services/user.service.ts
@@ -1,19 +1,19 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
+
+const AUTH_URL = env.AUTH_URL;
 
 export const userService = {
     getSession: async () => {
         try {
             const cookieStore = await cookies();
 
-            const res = await fetch(
-                "http://localhost:5000/api/auth/get-session",
-                {
-                    headers: {
-                        Cookie: cookieStore.toString(),
-                    },
-                    cache: "no-store",
+            const res = await fetch(`${AUTH_URL}/get-session`, {
+                headers: {
+                    Cookie: cookieStore.toString(),
                 },
-            );
+                cache: "no-store",
+            });
             const session = await res.json();
             if (session === null) {
                 return {
