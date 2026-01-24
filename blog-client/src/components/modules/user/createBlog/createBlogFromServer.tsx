970626a2987aded78c4c622c9c72ad23c9ad7da0
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-01-24 19:15:27 +0600

    module 35, vedio 3, done!

--- blog-client/src/components/modules/user/createBlog/createBlogFromServer.tsx
+++ blog-client/src/components/modules/user/createBlog/createBlogFromServer.tsx
@@ -9,12 +9,46 @@ import {
 } from "@/components/ui/card";
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { env } from "@/env";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { toast } from "sonner";
+
+const API_URL = env.API_URL
 
 export default function CreateBlogFromServer() {
     const createBlog = async (formData: FormData) => {
-		"use server";
-		console.log(formData.get("title"))
-	};
+        "use server";
+
+        const title = formData.get("title") as string;
+        const content = formData.get("content") as string;
+        const tags = formData.get("tags") as string;
+
+        const blogData = {
+            title,
+            content,
+            tags: tags
+                .split(",")
+                .map((item) => item.trim())
+                .filter((item) => item !== ""),
+        };
+        
+        const cookieStore = await cookies();
+        
+        const res = await fetch(`${API_URL}/posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: cookieStore.toString(),
+            },
+            body: JSON.stringify(blogData),
+        });
+
+        if(res.status === 201 && res.ok) {
+            redirect("/dashboard/create-blog?success");
+        }
+    };
 
     return (
         <Card className="max-w-xl mx-auto">
@@ -27,21 +61,38 @@ export default function CreateBlogFromServer() {
                     <FieldGroup>
                         <Field>
                             <FieldLabel>Title</FieldLabel>
-                            <Input type="text" name="title" />
+                            <Input
+                                type="text"
+                                name="title"
+                                placeholder="Blog Title"
+                                required
+                            />
                         </Field>
                         <Field>
                             <FieldLabel>Content</FieldLabel>
-							<TextArea name="content" />
+                            <Textarea
+                                name="content"
+                                placeholder="Write Your Blog"
+                                required
+                            />
                         </Field>
                         <Field>
-                            <FieldLabel>Tags</FieldLabel>
-                            <Input type="text" name="tags" />
+                            <FieldLabel>Tags (comma separeted)</FieldLabel>
+                            <Input
+                                type="text"
+                                name="tags"
+                                placeholder="nestjs, nextjs, typescript"
+                            />
                         </Field>
                     </FieldGroup>
                 </form>
             </CardContent>
             <CardFooter>
-                <Button form="blog-form" type="submit" className="w-full cursor-pointer">
+                <Button
+                    form="blog-form"
+                    type="submit"
+                    className="w-full cursor-pointer"
+                >
                     Submit
                 </Button>
             </CardFooter>
